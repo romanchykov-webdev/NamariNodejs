10443b28f256d311e00c8a7fd3d5d4f1a15c67cd
romanchykov-webdev <romanchykov.webdev@gmail.com>
2024-04-14 19:08:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const path = require("path");
+const { mimeTypes } = require("./utilities/mime");
+const { staticFile } = require("./utilities/static_file");
+const PORT = 3500;
+
+http
+  .createServer((req, res) => {
+    const url = req.url;
+    console.log(url);
+    console.log("server work");
+
+    switch (url) {
+      case "/":
+        staticFile(res, "/html/main_page.html", ".html");
+        break;
+      default:
+        const extname = String(path.extname(url)).toLocaleLowerCase();
+        if (extname in mimeTypes) staticFile(res, url, extname);
+        else {
+          res.statusCode = 404;
+          res.end();
+        }
+    }
+  })
+  .listen(PORT);
---
+++ utilities/static_file.js
@@ -0,0 +1,13 @@
+const { mimeTypes } = require("./mime");
+const fs = require("fs");
+module.exports.staticFile = function (res, filePath, ext) {
+  res.setHeader("Content-Type", mimeTypes[ext]);
+  fs.readFile("./public" + filePath, (error, data) => {
+    if (error) {
+      res.statusCode = 404;
+      res.end();
+      // statusCode404(res);
+    }
+    res.end(data);
+  });
+};
