bde0aeb3a32821ffd58f40e1197b7ba7a3967ee1
romanchykov-webdev <romanchykov.webdev@gmail.com>
2024-04-15 13:04:35 +0200

    res.end("<h1>Status Code 404 sam error</h1>");

--- index.js
+++ index.js
@@ -6,42 +6,21 @@ const PORT = 3500;
 
 http
   .createServer((req, res) => {
-    try {
-      const url = req.url;
-      console.log(url);
-      console.log("server work");
+    const url = req.url;
+    console.log(url);
+    console.log("server work");
 
-      switch (url) {
-        case "/":
-          // res.end("<h1>Main page new</h1>");
-          staticFile(res, "/html/main_page.html", ".html");
-          break;
-        default:
-          const extname = String(path.extname(url)).toLocaleLowerCase();
-          if (extname in mimeTypes) staticFile(res, url, extname);
-      }
-    } catch (e) {
-      // log.error(e);
-      res.statusCode = 404;
-      res.end("<h1>sam error 404</h1>");
-      return `sam error 404 ${e}`;
+    switch (url) {
+      case "/":
+        staticFile(res, "/html/main_page.html", ".html");
+        break;
+      default:
+        const extname = String(path.extname(url)).toLocaleLowerCase();
+        if (extname in mimeTypes) staticFile(res, url, extname);
+        else {
+          res.statusCode = 404;
+          res.end("<h1>Status Code 404 sam error</h1>");
+        }
     }
-
-    // const url = req.url;
-    // console.log(url);
-    // console.log("server work");
-    //
-    // switch (url) {
-    //   case "/":
-    //     staticFile(res, "/html/main_page.html", ".html");
-    //     break;
-    //   default:
-    //     const extname = String(path.extname(url)).toLocaleLowerCase();
-    //     if (extname in mimeTypes) staticFile(res, url, extname);
-    //     else {
-    //       res.statusCode = 404;
-    //       res.end();
-    //     }
-    // }
   })
   .listen(PORT || process.env.PORT);
